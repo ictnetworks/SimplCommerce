52b212d2741e423928f90ba0dfc30fbe5ffb38f1
ictnetworks <ictnet@ictnetworksystems.ca>
2020-04-28 00:19:16 -0400

    Create StaticData-CA.sql

---
+++ src/Database/StaticData-CA.sql
@@ -0,0 +1,15 @@
+INSERT INTO [Core_StateOrProvince] ([CountryId], [Code], [Name]) VALUES
+('CA', 'AB', 'Alberta', ''),
+('CA', 'ON', 'Ontario',''),
+('CA', 'BC', 'British Columbia', ''),
+('CA', 'NU', 'Nunavut', ''),
+('CA', 'QC', 'Quebec', ''),
+('CA', 'NL', 'Newfoundland Labrador', ''),
+('CA', 'PE', 'Prince Edward Island', ''),
+('CA', 'NS', 'Nova Scotia', ''),
+('CA', 'NB', 'New Brunswick', ''),
+('CA', 'NT', 'North West Territories', ''),
+('CA', 'YK', 'Yukon', ''),
+('US', 'SK', 'Saskatchewan', ''),
+('CA', 'MB', 'Manitoba', '')
+GO
